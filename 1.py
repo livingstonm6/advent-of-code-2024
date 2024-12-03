2ea9614685bd9c6431ae08734c6fd370c126e5dd
livingstonm6 <livingstonm6@mymacewan.ca>
2024-12-02 21:14:23 -0700

    Completed question 1.

---
+++ 1.py
@@ -0,0 +1,21 @@
+lst1 = []
+lst2 = []
+
+with open("inputs/input1.txt", "r") as file:
+    data = file.read()
+
+for line in data.splitlines():
+    temp = line.split("   ")
+    lst1.append(int(temp[0]))
+    lst2.append(int(temp[1]))
+
+lst1.sort()
+lst2.sort()
+
+total_dist = 0
+
+for i in range(len(lst1)):
+    total_dist += abs(lst1[i] - lst2[i])
+
+print(total_dist)
+
