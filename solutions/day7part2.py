d54ea5053f31e4fdad8c3d9e7d5997af73b091f2
livingstonm6 <livingstonm6@mymacewan.ca>
2024-12-12 12:37:34 -0700

    Day 7 part 2 complete.

---
+++ solutions/day7part2.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+with open("../inputs/input7.txt", "r") as file:
+    data = file.read()
+
+total = 0
+
+for line in data.splitlines():
+    temp = line.split(": ")
+    target = int(temp[0])
+    nums = deque([int(x) for x in temp[1].split(" ")])
+    valid = [False]
+
+    def recur():
+        if len(nums) == 1:
+            if nums[0] == target:
+                valid[0] = True
+            return
+
+        operand1 = nums.popleft()
+        operand2 = nums.popleft()
+
+        nums.appendleft(operand1 + operand2)
+        recur()
+        nums.popleft()
+
+        nums.appendleft(operand1 * operand2)
+        recur()
+        nums.popleft()
+
+        nums.appendleft(int(str(operand1) + str(operand2)))
+        recur()
+        nums.popleft()
+
+        nums.appendleft(operand2)
+        nums.appendleft(operand1)
+
+    recur()
+
+    if valid[0]:
+        total += target
+
+print(total)
