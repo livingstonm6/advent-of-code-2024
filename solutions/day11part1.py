444d1b2ae836742d81783ffcd43b14b89d2992c3
livingstonm6 <94208409+livingstonm6@users.noreply.github.com>
2024-12-15 02:54:17 -0700

    Optimized Day 11 part 1 solution using deque.

--- solutions/day11part1.py
+++ solutions/day11part1.py
@@ -1,23 +1,29 @@
+from collections import deque
+
 with open("../inputs/input11.txt", "r") as file:
     data = file.read().strip()
 
-stones = [int(x) for x in data.split(" ")]
+stones = deque([int(x) for x in data.split(" ")])
 
 for blink in range(25):
     i = 0
     print("blink", blink + 1 , "of 25")
-    while i < len(stones):
-        if stones[i] == 0:
-            stones[i] = 1
-            i += 1
-        elif len(str(stones[i])) % 2 == 0:
-            midpoint = len(str(stones[i])) // 2
-            stone1 = int(str(stones[i])[:midpoint])
-            stone2 = int(str(stones[i])[midpoint:])
-            stones = stones[:i] + [stone1, stone2] + stones[i+1:]
-            i += 2
+    length = len(stones)
+    while i < length:
+        stone = stones.popleft()
+        if stone == 0:
+            stones.append(1)
         else:
-            stones[i] = stones[i] * 2024
-            i += 1
+            s = str(stone)
+            if len(s) % 2 == 0:
+                midpoint = len(s) // 2
+                stone1 = int(s[:midpoint])
+                stone2 = int(s[midpoint:])
+                stones.append(stone1)
+                stones.append(stone2)
+            else:
+                stones.append(stone * 2024)
+
+        i += 1
 
 print(len(stones))
